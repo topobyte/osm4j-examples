819747d5465454613f87d10f1cc6a2e25aedcd83
Sebastian Kürten <sebastian@topobyte.de>
2018-05-09 14:12:05 +0200

    MapRendering: remove duplicate semicolon

--- src/main/java/de/topobyte/osm4j/examples/tutorial/MapRendering.java
+++ src/main/java/de/topobyte/osm4j/examples/tutorial/MapRendering.java
@@ -112,7 +112,7 @@ public class MapRendering extends JPanel
 
 	// Some fields that define the map colors and street line widths
 	private Color cBackground = new Color(0xEEEEEE);
-	private Color cBBox = Color.BLUE;;
+	private Color cBBox = Color.BLUE;
 	private Color cStreetForeground = Color.WHITE;
 	private Color cStreetBackground = new Color(0xDDDDDD);
 	private Color cStreetText = Color.BLACK;
